b9541ac99e9337f708a5173ded58104dc26d29ab
Max Faupel <max.faupel@gmx.de>
2021-04-10 23:12:07 +0200

    implement delete todo

--- src/main/java/com/example/todoservice/todo/TodoController.java
+++ src/main/java/com/example/todoservice/todo/TodoController.java
@@ -2,7 +2,9 @@ package com.example.todoservice.todo;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:4200")
 public class TodoController {
     private TodoService todoService;
-    
+
     public TodoController(TodoService todoService) {
         this.todoService = todoService;
     }
-    
+
     @GetMapping("/users/{username}/todos")
     public List<Todo> getTodos(@PathVariable String username) {
         return todoService.findByUsername(username);
+    }
+
+    @DeleteMapping(("/users/{username}/todos/{id}"))
+    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable Long id) {
+        if (todoService.deleteTodoById(id)) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/example/todoservice/todo/TodoService.java
+++ src/main/java/com/example/todoservice/todo/TodoService.java
@@ -3,6 +3,7 @@ package com.example.todoservice.todo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,5 +18,14 @@ public class TodoService {
         List<Todo> todos = new ArrayList<>();
         todoRepository.findByUsername(username).forEach(todo -> todos.add(todo));
         return todos;
+    }
+
+    public boolean deleteTodoById(Long id) {
+        try {
+            todoRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            return false;
+        }
+        return true;
     }
 }
