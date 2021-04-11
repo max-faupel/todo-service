1c9c3e1e678b5a4e0f4b3b1260ef2a7db3299618
Max Faupel <max.faupel@gmx.de>
2021-04-11 23:29:28 +0200

    fixed welcome service

--- src/main/java/com/example/todoservice/TodoServiceApplication.java
+++ src/main/java/com/example/todoservice/TodoServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.todoservice.todo;
+package com.example.todoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/example/todoservice/welcome/WelcomeController.java
+++ src/main/java/com/example/todoservice/welcome/WelcomeController.java
@@ -1,21 +1,22 @@
 package com.example.todoservice.welcome;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/welcome")
 public class WelcomeController {
-    @RequestMapping(method = RequestMethod.GET, path = "/welcome")
+    @GetMapping
     public WelcomeBean getWelcomeMessage() {
-        throw new RuntimeException("Error happened in REST service!");
-        // return new WelcomeBean("Welcome from REST service!");
+        // throw new RuntimeException("Error happened in REST service!");
+        return new WelcomeBean("Welcome from REST service!");
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "/welcome/{username}")
+    @GetMapping("/{username}")
     public WelcomeBean getWelcomeMessageForUser(@PathVariable String username) {
         return new WelcomeBean(String.format("Welcome %s from REST service", username));
     }
