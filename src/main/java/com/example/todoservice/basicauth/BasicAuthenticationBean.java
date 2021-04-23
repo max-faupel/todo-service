218a25e5ef2076ae7e045fce7d68726edb6a1d8c
Max Faupel <max.faupel@gmx.de>
2021-04-23 22:42:31 +0200

    implement basic auth service

---
+++ src/main/java/com/example/todoservice/basicauth/BasicAuthenticationBean.java
@@ -0,0 +1,17 @@
+package com.example.todoservice.basicauth;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class BasicAuthenticationBean {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
---
+++ src/main/java/com/example/todoservice/basicauth/BasicAuthenticationController.java
@@ -0,0 +1,17 @@
+package com.example.todoservice.basicauth;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/basicauth")
+public class BasicAuthenticationController {
+    @GetMapping
+    public BasicAuthenticationBean authenticate() {
+        return new BasicAuthenticationBean("You are authenticated!");
+    }
+
+}
